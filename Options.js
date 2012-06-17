41c4b7c0ca05c5bd315e208dc3fa43dbdbde747d
rolandpoulter <rolandpoulter@gmail.com>
2012-06-17 16:56:08 -0700

    added tests

--- Options.js
+++ Options.js
@@ -2,36 +2,36 @@
 
 "use strict";
 
-var merge = require('mrg');
+var util = {
+	merge: require('mrg'),
 
-function deepCreate (obj) {
-	var created = Object.create(obj),
-	    prop;
+	deepCreate: function deepCreate (object) {
+		var created = Object.create(object),
+		    property;
 
-	for (prop in obj) {
-		if (obj.hasOwnProperty(prop) && typeof obj[prop] === 'object') {
-			created[prop] = deepCreate(obj[prop]);
+		for (property in object) {
+			if (object.hasOwnProperty(property) && typeof object[property] === 'object') {
+				created[property] = deepCreate(object[property]);
+			}
 		}
-	}
 
-	return created;
+		return created;
+	}
 }
 
-var Options =
-
-module.exports = require('clss')('Options', function (def) {
+var Options = module.exports = require('clss')('Options', function (def) {
 	def.setOptions = function (options) {
 		this.options = this.options &&
 			this.options !== this.constructor.prototype.options ?
 				this.options : this.options ? Object.create(this.options) : {};
 
-		if (options) merge(this.options, options);
+		if (options) util.merge(this.options, options);
 
 		return this;
 	};
 
 	def.newOptions = function (options) {
-		this.options = deepCreate(this.options || (this.options = {}));
+		this.options = util.deepCreate(this.options || (this.options = {}));
 
 		return this.setOptions(options);
 	};
@@ -39,7 +39,4 @@ module.exports = require('clss')('Options', function (def) {
 
 Options.create(Options);
 
-Options.util = {
-	deepCreate: deepCreate,
-	merge: merge
-};
+Options.util = util;
--- test.js
+++ test.js
@@ -4,6 +4,94 @@ module.exports = require('spc').describe('Options', function () {
 	before(function () {
 		should();
 	});
+
+	describe('util#deepCreate', function () {
+		beforeEach(function () {
+			sinon.spy(Object, 'create');
+			this.object = {};
+			this.result = Options.util.deepCreate(this.object);
+		});
+
+		afterEach(function () {
+			Object.create.restore();
+		});
+
+		it('should call Object.create', function () {
+			Object.create.called.should.be.ok;
+		});
+	});
+
+	describe('#setOptions', function () {
+		beforeEach(function () {
+			sinon.spy(Options.util, 'merge');
+			this.subject = {};
+			this.options = {};
+			this.result = Options.setOptions.call(this.subject, this.options);
+		});
+
+		afterEach(function () {
+			Options.util.merge.restore();
+		});
+
+		it('should call merge', function () {
+			Options.util.merge.called.should.be.ok;
+		});
+	});
+
+	describe('#newOptions', function () {
+		beforeEach(function () {
+			sinon.spy(Options.util, 'deepCreate');
+			this.subject = {
+				options: this.originalOptions = {},
+				setOptions: sinon.spy(function () {return this;})
+			};
+			this.options = {};
+			this.result = Options.newOptions.call(this.subject, this.options);
+		});
+
+		afterEach(function () {
+			Options.util.deepCreate.restore();
+		});
+
+		it('should call deepCreate', function () {
+			var deepCreate = Options.util.deepCreate;
+			deepCreate.called.should.be.ok;
+			deepCreate.args[0][0].should.equal(this.originalOptions);
+		});
+
+		it('should set the options', function () {
+			var setOptions = this.subject.setOptions;
+			setOptions.called.should.be.ok;
+			setOptions.args[0][0].should.equal(this.options);
+		});
+
+		it('should have options derived from the original options', function () {
+			Options.util.deepCreate.
+				returnValues[0].should.equal(this.subject.options);
+		});
+
+		it('should return the subject', function () {
+			this.result.should.equal(this.subject);
+		});
+	});
+
+	describe('instance', function () {
+		beforeEach(function () {
+			this.subject = new Options();
+		});
+
+		it('should be an instance of Options', function () {
+			(this.subject instanceof Options).should.be.ok;
+		});
+
+		it('should have #setOptions', function () {
+			this.subject.setOptions.should.equal(Options.prototype.setOptions);
+		});
+
+		it('should have #newOptions', function () {
+			this.subject.newOptions.should.equal(Options.prototype.newOptions);
+		});
+	});
 });
 
 require('spc/reporter/dot')(module.exports);
